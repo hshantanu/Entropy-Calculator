93cc8c18bf6669e65d99268cd7f8a2a705bd0556
Shantanu Hirlekar <2hshantanu@gmail.com>
2017-04-17 12:19:39 -0400

    Add files via upload
    Cleaned the code and added comments.

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,7 @@
+
+//Shantanu Hirlekar
+//Malware Analysis Mini Project Spring(2017)
+
 //I have referred code from http://rosettacode.org/wiki/Entropy#C.2B.2B 
 //I used this to refer the formula. 
 
@@ -16,6 +20,7 @@
 using namespace std;
 typedef unsigned char BYTE;
 
+//Lists the .exe and .dll files present in a directory. 
 vector<string> listFiles(const char *searchpath) {
 
 	vector<string> names;
@@ -26,7 +31,6 @@ vector<string> listFiles(const char *searchpath) {
 	
 	while (handle != INVALID_HANDLE_VALUE)
 	{
-		//cout << "\n" << search_data.cFileName;
 		names.push_back(search_data.cFileName);
 		if (FindNextFileA(handle, sd) == FALSE)
 			break;
@@ -36,14 +40,62 @@ vector<string> listFiles(const char *searchpath) {
 	return names;
 }
 
+//Prints and returns entropy of a file. 
+double getEntropy(FILE *file) {
+
+	long fileSize = 0;
+	BYTE *fileBuf;
+	if (file)
+	{
+		int fileNum = _fileno(file);
+		HANDLE hFile = (HANDLE)_get_osfhandle(fileNum);
+		fileSize = GetFileSize(hFile, NULL);
+	}
+
+	fileBuf = new BYTE[fileSize];
+	fread(fileBuf, fileSize, 1, file);
+
+	double entropy = 0;
+	map<char, int> freq;
+	cout << "Getting the entropy of the file, please wait....." << endl;
+	for (int i = 0; i < fileSize; i++) {
+		freq[fileBuf[i]]++;
+	}
+	for (pair<char, int> p : freq)
+	{
+		double freq = static_cast<double>(p.second) / fileSize;
+		entropy += freq * log2(freq);
+	}
+	entropy *= -1;
+	cout << "Entropy is: " << entropy << endl;
+	return entropy;
+}
+
+//A function which returs the files having entropy in top 10%
+void getTen(float cut_off, map<string, float> track)
+{
+	cout <<endl<< "Cut off for the 10% is: " << cut_off << endl;
+	cout << "The files having entropy in the top 10% are: "<<endl;
+	for (auto val : track) {
+		if (val.second >= cut_off)
+			cout << val.first << ": " << val.second << endl;
+	}
+}
+
 int main(int argc, char const *argv[])
 {
-	int i;
-	BYTE *fileBuf;			// Pointer to our buffered data
-	FILE *file = NULL;		// File pointer
-	long fileSize = 0;
-	const char *filepath;	
+	int i;			
+	FILE *file = NULL;		
+	const char *filepath;
+	map<string, float> track;
+	float entropy_sum = 0;
+	float max = 0;
+	const char *exe = "/*.exe";
+	const char *dll = "/*.dll";
+	char result1[100];
+	char result2[100];
 	
+	//Open the file provided in the command line argument. Else go o the default path. 
 	if (argc != 2) {
 		filepath = "C:/WINDOWS/System32/";
 	}
@@ -55,13 +107,8 @@ int main(int argc, char const *argv[])
 		cout << endl << "Something has gone wrong opening the directory!" << endl;
 		exit(1);
 	}
-	map<string, float> track;
-	float entropy_sum = 0;
-	float max = 0;
-	const char *exe = "/*.exe";
-	const char *dll = "/*.dll";
-	char result1[100];
-	char result2[100];
+	
+	//call the listFiles function for the .exe and .dll files respectively. 
 	strcpy(result1, filepath);
 	strcat(result1, exe);
 	strcpy(result2, filepath);
@@ -70,6 +117,8 @@ int main(int argc, char const *argv[])
 	int exe_size = names.size();
 	vector<string> names2 = listFiles(result2);
 	int dll_size = names2.size();
+
+	//combining the vectors of .exe and .dll
 	names.insert(names.end(), names2.begin(), names2.end());
 	int total_size = exe_size + dll_size;
 
@@ -77,11 +126,15 @@ int main(int argc, char const *argv[])
 	cout << "Number of .dll files are: " << dll_size << endl;
 	cout << "Total files are: " << total_size << endl;
 
+	//Go to the specified path of the directory.
 	if (SetCurrentDirectoryA(filepath))
-		cout << "Going to the filepath." << endl;
-	else
-		cout << "Can't go to the filepath." << endl;
-	
+		cout <<endl<< "Going to the filepath." << endl;
+	else {
+		cout <<endl<< "Can't go to the filepath." << endl;
+		exit(1);
+	}
+
+	//Open each file and calculate it's entropy.
 	for(string s : names){
 		
 		if ((file = fopen(s.c_str(), "rb")) == NULL) {
@@ -91,46 +144,23 @@ int main(int argc, char const *argv[])
 		else
 			cout << endl << "File opened successfully" << endl;
 
-		if (file)
-		{
-			int fileNum = _fileno(file);
-			HANDLE hFile = (HANDLE)_get_osfhandle(fileNum);
-			fileSize = GetFileSize(hFile, NULL);
-		}
+		double entropy = getEntropy(file);
 
-		// Allocate space in the buffer for the whole file
-		fileBuf = new BYTE[fileSize];
-
-		// Read the file in to the buffer
-		fread(fileBuf, fileSize, 1, file);
-
-		double entropy = 0;
-		map<char, int> freq;
-		cout << "Getting the entropy of the file, please wait....." << endl;
-		for (int i = 0; i < fileSize; i++) {
-			freq[fileBuf[i]]++;
-		}
-		for (pair<char, int> p : freq)
-		{
-			double freq = static_cast<double>(p.second) / fileSize;
-			entropy += freq * log2(freq);
-		}
-		entropy *= -1;
-		cout << "Entropy is: " << entropy << endl;
 		entropy_sum += entropy;
 		if (entropy > max)
 			max = entropy;
 		track[s] = entropy;
 		fclose(file);
 	}
+
+	//Printing the average entropy of the files. 
 	float avg = entropy_sum / total_size;
+	cout <<endl<< "The average entropy of all the files is: " << avg << endl;
+
+	//Getting the files with entropy in top 10%
 	float cut_off = max * 0.9;
-	cout << "Cut off is: " << cut_off << endl;
-	for (auto val : track) {
-		if (val.second >= cut_off)
-			cout << val.first << ": " << val.second << endl;
-	}
-	cout << "The average entropy of all the files is: " << avg << endl;
+	getTen(cut_off, track);	
+	
 	cin >> i;
 	return 0;
 }
